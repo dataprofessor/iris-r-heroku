c3df641ef3157cab76f336f0eca5484a6dae1922
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2020-06-27 22:29:50 +0700

    Add files via upload

---
+++ server.R
@@ -0,0 +1,55 @@
+library(data.table)
+library(randomForest)
+
+# Read in the RF model
+model <- readRDS("model.rds")
+
+# Training set
+TrainSet <- read.csv("training.csv", header = TRUE)
+TrainSet <- TrainSet[,-1]
+
+shinyServer(function(input, output, session) {
+
+  # Input Data
+  datasetInput <- reactive({
+
+    df <- data.frame(
+      Name = c("Sepal Length",
+               "Sepal Width",
+               "Petal Length",
+               "Petal Width"),
+      Value = as.character(c(input$Sepal.Length,
+                             input$Sepal.Width,
+                             input$Petal.Length,
+                             input$Petal.Width)),
+      stringsAsFactors = FALSE)
+
+    Species <- 0
+    df <- rbind(df, Species)
+    input <- transpose(df)
+    write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+    test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
+
+    Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
+    print(Output)
+
+  })
+
+  # Status/Output Text Box
+  output$contents <- renderPrint({
+    if (input$submitbutton>0) {
+      isolate("Calculation complete.")
+    } else {
+      return("Server is ready for calculation.")
+    }
+  })
+
+  # Prediction results table
+  output$tabledata <- renderTable({
+    if (input$submitbutton>0) {
+      isolate(datasetInput()) 
+    }
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,34 @@
+library(shiny)
+
+pageWithSidebar(
+
+  # Page header
+  headerPanel('Iris Predictor'),
+
+  # Input values
+  sidebarPanel(
+    HTML("<h3>Input parameters</h4>"),
+    sliderInput("Sepal.Length", label = "Sepal Length", value = 5.0,
+                min = min(TrainSet$Sepal.Length),
+                max = max(TrainSet$Sepal.Length)
+    ),
+    sliderInput("Sepal.Width", label = "Sepal Width", value = 3.6,
+                min = min(TrainSet$Sepal.Width),
+                max = max(TrainSet$Sepal.Width)),
+    sliderInput("Petal.Length", label = "Petal Length", value = 1.4,
+                min = min(TrainSet$Petal.Length),
+                max = max(TrainSet$Petal.Length)),
+    sliderInput("Petal.Width", label = "Petal Width", value = 0.2,
+                min = min(TrainSet$Petal.Width),
+                max = max(TrainSet$Petal.Width)),
+
+    actionButton("submitbutton", "Submit", class = "btn btn-primary")
+  ),
+
+  mainPanel(
+    tags$label(h3('Status/Output')), # Status/Output Text Box
+    verbatimTextOutput('contents'),
+    tableOutput('tabledata') # Prediction results table
+
+  )
+)
